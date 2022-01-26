966833ffc5652c914094a7fad25a25377eb850ed
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-26 15:42:33 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -5,11 +5,11 @@ import time
 def expensive_computation(a, b):
     # 👇 Added this
     st.write("Cache miss: expensive_computation(", a, ",", b, ") ran")
-    time.sleep(2)  # This makes the function take 2s to run
+    time.sleep(5)  # This makes the function take 2s to run
     return a * b
 
 a = 2
-b = 21
+b = 20
 res = expensive_computation(a, b)
 
 st.write("Result:", res)
