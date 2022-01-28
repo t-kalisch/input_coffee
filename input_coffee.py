9ae59f5fbc9786998880d4c2435b2a2edc829eec
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 15:59:06 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import datetime
+from datetime import datetime
 from calculations import *
 
 st.set_page_config(page_title="Input Coffee",page_icon="chart_with_upwards_trend",layout="wide")
