3862250bf62929d8e6deb50c2fbbe6b3e1349272
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 15:43:48 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -4,7 +4,7 @@ from calculations import *
 
 st.set_page_config(page_title="Input Coffee",page_icon="chart_with_upwards_trend",layout="wide")
 logged_in = False
-header,buf1 = st.columns([1,0.1])
+header,buf1 = st.columns([1000,0.00001])
 
 @st.cache
 def check_login(user, user_pw):
@@ -20,8 +20,8 @@ def check_login(user, user_pw):
 
 
 col1,col2,col3 = st.columns([0.5,1,0.7])
-user = col1.text_input(label="", placeholder="Username")
-user_pw = col1.text_input(label="", type="password", placeholder="Password")
-login = col1.checkbox("Login", help="You are logged in while this checkbox is ticked")
+user = col2.text_input(label="", placeholder="Username")
+user_pw = col2.text_input(label="", type="password", placeholder="Password")
+login = col2.checkbox("Login", help="You are logged in while this checkbox is ticked")
 if login:
   check_login(user, user_pw)
