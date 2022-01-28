a6842eb8fdd61624d712d028ce62ef19e0aac83a
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 17:13:55 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -8,11 +8,18 @@ buf1,header2,buf2 = st.columns([0.5,1,0.7])
 
 @st.cache(suppress_st_warning=True)
 def check_login(user, user_pw):
-    logged_in = False
+    logged_in=[]
     user_data = get_user_data()
     for i in range(len(user_data)):
         if user == user_data[i][0] and user_pw == user_data[i][1]:
-            logged_in = True
+            logged_in.append(1)
+            if user_data[i][2] == 1:
+                logged_in.append(1)
+            else:
+                logged_in.append(0)
+        else:
+            logged_in.append(0)
+            logged_in.append(0)
     return logged_in
         
 def submit_coffee(user):
@@ -27,25 +34,33 @@ user_pw = col2.text_input(label="", type="password", placeholder="Password")
 login = col2.checkbox("Login", help="You are logged in while this checkbox is ticked")
 if login:
     logged_in = check_login(user, user_pw)
-    if logged_in == True:
+    if logged_in[0] == 1:
         header2.markdown("Logged in as "+user)
     else:
         header2.markdown("Incorrect username or password")
 else:
     header2.markdown("Please log in to submit a coffee")
 
-if login and logged_in == True:
+if login and logged_in[0] == 1:
     now = datetime.datetime.now()
     break_length = check_breakstatus(now)
     minutes = str(int(break_length.seconds/60))
-    seconds = break_length.seconds-(60*int(break_length.seconds/60))
+    strseconds = break_length.seconds-(60*int(break_length.seconds/60))
     if seconds < 10:
         seconds = "0"+str(seconds)
     if break_length.total_seconds() < 900:
-        col2.markdown("A coffee break is under way since "+str(minutes)+":"+str(seconds)+".")
+        col2.markdown("A coffee break is under way since "+str(minutes)+":"+strseconds+".")
         col2.button("Update")
-        submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = submit_coffee(user))
+        if logged_in[1] == 1:
+            name = col2.text_input("Drinker", placeholder = "Username")
+            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = submit_coffee(user))
+        else:
+            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = submit_coffee(user))
     elif break_length.total_seconds() >= 900:
         col2.markdown("No coffee break is currently under way.")
         col2.button("Update")
-        submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = submit_coffee(user))
+        if logged_in[1] == 1:
+            name = col2.text_input("Drinker", placeholder = "Username")
+            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = submit_coffee(user))
+        else:
+            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = submit_coffee(user))
