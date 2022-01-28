48c30739f252e149362f61889325027dae769be0
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 15:15:29 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -6,5 +6,5 @@ st.set_page_config(page_title="Input Coffee",page_icon="chart_with_upwards_trend
 
 col1,col2 = st.columns([1,1])
 user = col1.text_input(label="", placeholder="Username")
-user_pw = col2.text_input(label="", type="password", placeholder="Password")
+user_pw = col1.text_input(label="", type="password", placeholder="Password")
 login = col1.checkbox("Login", help="You are logged in while this checkbox is ticked")
