9b150892546ad7348e845517b92a4ffa39bcb3f2
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 16:27:19 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -37,7 +37,7 @@ if login and logged_in == True:
     break_length = check_breakstatus(now)
     st.write(check_breakstatus(now).total_seconds())
     if break_length.total_seconds() < 900:
-        st.markdown("A coffee break is under way since "+str(int(break_length.seconds/60))+":"+str(break_length.seconds))
+        st.markdown("A coffee break is under way since "+str(int(break_length.seconds/60))+":"+str(break_length.seconds)-(60*int(break_length.seconds/60)))
         submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.")
     elif break_length.total_seconds() >= 900:
         submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.")
