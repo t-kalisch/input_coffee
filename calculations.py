53e4aeac273d8b59c0a8f189a07fdc0b909ec9b2
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-28 15:58:30 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,7 +1,11 @@
 import mysql.connector as mysql
 import streamlit as st
+import datetime
 
 @st.cache
 def get_user_data():
 	user_data=[['TK', 'akstr!admin2',1],['PB','akstr!admin2',1],['NV',None,None],['DB',None,None],['FLG','baddragon',None],['SHK',None,None],['TB',None,None],['TT',None,None],['RS',None,None]]
 	return user_data
+
+def check_breakstatus(now):
+	return now
