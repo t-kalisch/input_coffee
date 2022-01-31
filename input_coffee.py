e8d9d5e8611cc027d030d2937f27645c0bab99d1
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-31 16:19:34 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -17,6 +17,10 @@ def check_login(user, user_pw):
                 logged_in[1] = 1
     return logged_in
 
+def on_click_action:
+    st.write("Button clicked")
+
+
 
 col1,col2,col3 = st.columns([0.5,1,0.7])
 user = col2.text_input(label="", placeholder="Username")
@@ -44,14 +48,14 @@ if login and logged_in[0] == 1:
         col2.button("Update")
         if logged_in[1] == 1:
             name = col2.text_input("Drinker", placeholder = "Username")
-            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = submit_coffee(user, name, logged_in))
+            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = on_click_action())#submit_coffee(user, name, logged_in))
         else:
-            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = submit_coffee(user, "", logged_in))
+            submit_coffee = col2.button("Add coffee to coffee break", help = "A break is under way. Join it by adding a coffee here.", on_click = on_click_action())#submit_coffee(user, "", logged_in))
     elif break_length.total_seconds() >= 900:
         col2.markdown("No coffee break is currently under way.")
         col2.button("Update")
         if logged_in[1] == 1:
             name = col2.text_input("Drinker", placeholder = "Username")
-            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = submit_coffee(user, name, logged_in))
+            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = on_click_action())#submit_coffee(user, name, logged_in))
         else:
-            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = submit_coffee(user, "", logged_in))
+            submit_coffee = col2.button("Start a coffee break", help = "Start a break and add a coffee to your name here.", on_click = on_click_action())#submit_coffee(user, "", logged_in))
