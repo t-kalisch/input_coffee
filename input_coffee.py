91fc1f43327462f28c9f11dd67c8aade3426022b
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-03-02 14:47:34 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -78,6 +78,7 @@ if st.session_state.logged_in == True:
     seconds = break_length.seconds-(60*int(break_length.seconds/60))
     strseconds = str(seconds)
     st.write(break_length)
+    st.write(break_length.total_seconds())
     if seconds < 10:
         strseconds = "0"+str(seconds)
     if break_length.total_seconds() < 900:
