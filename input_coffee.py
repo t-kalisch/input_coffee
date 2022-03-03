e4cbb9d79d8b51876fbb90d7f49280e3f0de027a
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-03-03 14:51:49 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -61,20 +61,20 @@ col1,col2,col3,col4 = st.columns([0.5,0.6,0.4,0.7])
 if st.session_state.logged_in == True:
     logout = col2.button("Logout", help="Click here to log out", key="logout_button")
 else:
-    login = col2.button("Login", help="Click here to log in", key="login_button", on_click=check_login(user, user_pw))
+    login = col2.button("Login", help="Click here to log in", key="login_button")#, on_click=check_login(user, user_pw))
     
 col3.checkbox("Remember me", help="Keep me logged in")
 
 
 
-#if st.session_state.logged_in == True:
-#    if logout:
-#        st.session_state.logged_in=False
-#        st.session_state.attempt=False
-#        st.session_state.admin=False
-#else:
-#    if login: 
-#        check_login(user, user_pw)
+if st.session_state.logged_in == True:
+    if logout:
+        st.session_state.logged_in=False
+        st.session_state.attempt=False
+        st.session_state.admin=False
+else:
+    if login: 
+        check_login(user, user_pw)
             
         
 if st.session_state.logged_in == True and st.session_state.attempt == False:
