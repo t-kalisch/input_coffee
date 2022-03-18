b40bb774ead128be1739dde9f1d2f88f0bad5ba6
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-03-18 11:26:43 +0100

    Update input_coffee.py

--- input_coffee.py
+++ input_coffee.py
@@ -27,7 +27,7 @@ if 'admin' not in st.session_state:
 if cookie_manager.get(cookie="logged_in") == "true":
     st.session_state.logged_in="true"
     st.session_state.user = cookie_manager.get(cookie="user")
-    st.session_state.admin=cookie_manager.get(cookie="status")
+    st.session_state.admin=cookie_manager.get(cookie="admin")
 
 
 #logged_in=st.session_state.logged_in
@@ -62,10 +62,10 @@ def check_login(user, user_pw):
         #if remember_me:
         #    cookie_manager.set("logged_in", st.session_state.logged_in, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_true")
         #    cookie_manager.set("user", st.session_state.user, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_user")
-        #    cookie_manager.set("status", st.session_state.admin, expires_at=datetime.datetime(year=2030, month=1, day=1), key="admin_status")
+        #    cookie_manager.set("admin", st.session_state.admin, expires_at=datetime.datetime(year=2030, month=1, day=1), key="admin_status")
         #else:
         #    cookie_manager.set("logged_in", False, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logout")
-        #    cookie_manager.set("status", None, expires_at=datetime.datetime(year=2030, month=1, day=1), key="del_admin_status")
+        #    cookie_manager.set("admin", None, expires_at=datetime.datetime(year=2030, month=1, day=1), key="del_admin_status")
         #    cookie_manager.set("user", None, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_user") 
     else:
         st.session_state.logged_in=False
